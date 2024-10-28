c32fe040b6052952050b0c2973ea3ca35ca54776
Lab1_Kotlin <hieppvph30897@fpt.edu.vn>
2024-10-28 23:56:40 +0700

    Hiep Sign_in java

--- app/src/main/java/com/example/md06_clothes/SignInActivity.java
+++ app/src/main/java/com/example/md06_clothes/SignInActivity.java
@@ -1,6 +1,14 @@
 package com.example.md06_clothes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +17,47 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignInActivity extends AppCompatActivity {
+    private boolean isPasswordVisible = false;
+    private EditText edt_passin, edt_mailin;
+    private ImageView hidein;
+    private Button btn_signin;
+    private TextView txtsignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        hidein = findViewById(R.id.hidein);
+        edt_passin = findViewById(R.id.edt_matkhau_user);
+        btn_signin = findViewById(R.id.btn_signin);
+        txtsignup = findViewById(R.id.txtSignup);
+
+
+        hidein = findViewById(R.id.hidein);
+        edt_passin = findViewById(R.id.edt_matkhau_user);
+        btn_signin = findViewById(R.id.btn_signin);
+        txtsignup = findViewById(R.id.txtSignup);
+        hidein.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isPasswordVisible) {
+                    edt_passin.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                    hidein.setImageResource(R.drawable.hide);
+                } else {
+                    edt_passin.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                    hidein.setImageResource(R.drawable.hideon);
+                }
+                isPasswordVisible = !isPasswordVisible;
+                edt_passin.setSelection(edt_passin.getText().length());
+            }
+        });
+        txtsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this,MainActivity.class));
+            }
         });
     }
 }

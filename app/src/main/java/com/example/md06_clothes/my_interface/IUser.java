1d32da2a197346f77fddb24237d3c5300a34f3c8
beckn <beckneee@gmail.com>
2024-10-30 20:07:08 +0700

    User interface model adapter

---
+++ app/src/main/java/com/example/md06_clothes/my_interface/IUser.java
@@ -0,0 +1,10 @@
+package com.example.md06_clothes.my_interface;
+
+public interface IUser {
+
+    void OnSucess();
+
+    void OnFail();
+
+    void getDataUser(String id, String email,String name, String address, String phone, String date, String gender, String avatar);
+}

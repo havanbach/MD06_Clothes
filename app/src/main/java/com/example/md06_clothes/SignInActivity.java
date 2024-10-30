74b388862dbb3f95eb45cc1a8cd844401a8c74e9
beckn <beckneee@gmail.com>
2024-10-30 20:37:38 +0700

    SignIn update

--- app/src/main/java/com/example/md06_clothes/SignInActivity.java
+++ app/src/main/java/com/example/md06_clothes/SignInActivity.java
@@ -1,10 +1,15 @@
 package com.example.md06_clothes;
 
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,6 +20,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SignInActivity extends AppCompatActivity {
     private boolean isPasswordVisible = false;
@@ -22,23 +30,40 @@ public class SignInActivity extends AppCompatActivity {
     private ImageView hidein;
     private Button btn_signin;
     private TextView txtsignup;
+    private Button btnDangNhap;
+    private TextView btnDangKy;
+    private EditText edtEmailUser, edtMatKhauUser;
+    private TextView tvForgotPassword;
+    private CircleImageView cirDangNhapFacebook, cirDangNhapGoogle;
+    private static final int TIME_DELAY = 2000;
+    private static long back_pressed;
+    private String sosanh;
+    private ProgressDialog progressDialog;
+
+    // Check Internet
+    private BroadcastReceiver MyReceiver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
 
         hidein = findViewById(R.id.hidein);
         edt_passin = findViewById(R.id.edt_matkhau_user);
-        btn_signin = findViewById(R.id.btn_signin);
-        txtsignup = findViewById(R.id.txtSignup);
-
+        btnDangNhap = findViewById(R.id.btn_dangnhap);
+        btnDangKy = findViewById(R.id.btn_dangky);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        InitWidget();
+//        MyReceiver = new MyReceiver();      // Check Internet
+        broadcastIntent();                  // Check Internet
+//        if (NetworkUtil.isNetworkConnected(this)){
+//            Event();
+//        }
 
         hidein = findViewById(R.id.hidein);
         edt_passin = findViewById(R.id.edt_matkhau_user);
-        btn_signin = findViewById(R.id.btn_signin);
-        txtsignup = findViewById(R.id.txtSignup);
+        btn_signin = findViewById(R.id.btn_dangnhap);
+        txtsignup = findViewById(R.id.btn_dangky);
         hidein.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -67,5 +92,22 @@ public class SignInActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private void InitWidget() {
+        btnDangNhap = findViewById(R.id.btn_dangnhap);
+        btnDangKy = findViewById(R.id.btn_dangky);
+        edtEmailUser = findViewById(R.id.edt_email_user);
+        edtMatKhauUser = findViewById(R.id.edt_matkhau_user);
+        tvForgotPassword = findViewById(R.id.tv_forgot_password);
+        progressDialog = new ProgressDialog(this);
+    }
+    public void broadcastIntent() {
+        registerReceiver(MyReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(MyReceiver);
     }
 }

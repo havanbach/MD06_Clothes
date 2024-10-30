59441985a36c5fa6540616cf9aff3f1049213fc1
beckn <beckneee@gmail.com>
2024-10-30 21:09:41 +0700

    SignIn oke and layout admin home

--- app/src/main/java/com/example/md06_clothes/SignInActivity.java
+++ app/src/main/java/com/example/md06_clothes/SignInActivity.java
@@ -4,6 +4,7 @@ import android.app.ProgressDialog;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.metrics.Event;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.text.method.HideReturnsTransformationMethod;
@@ -14,13 +15,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import com.example.md06_clothes.Models.Admin;
+import com.example.md06_clothes.View.Admin.AdminHomeActivity;
+import com.example.md06_clothes.ultil.MyReceiver;
+import com.example.md06_clothes.ultil.NetworkUtil;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -52,18 +72,18 @@ public class SignInActivity extends AppCompatActivity {
         edt_passin = findViewById(R.id.edt_matkhau_user);
         btnDangNhap = findViewById(R.id.btn_dangnhap);
         btnDangKy = findViewById(R.id.btn_dangky);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         InitWidget();
-//        MyReceiver = new MyReceiver();      // Check Internet
+        MyReceiver = new MyReceiver();      // Check Internet
         broadcastIntent();                  // Check Internet
-//        if (NetworkUtil.isNetworkConnected(this)){
-//            Event();
-//        }
+        if (NetworkUtil.isNetworkConnected(this)) {
+            Event();
+        }
 
         hidein = findViewById(R.id.hidein);
         edt_passin = findViewById(R.id.edt_matkhau_user);
-        btn_signin = findViewById(R.id.btn_dangnhap);
-        txtsignup = findViewById(R.id.btn_dangky);
+        btnDangNhap = findViewById(R.id.btn_dangnhap);
+        btnDangKy = findViewById(R.id.btn_dangky);
         hidein.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,19 +98,102 @@ public class SignInActivity extends AppCompatActivity {
                 edt_passin.setSelection(edt_passin.getText().length());
             }
         });
-        txtsignup.setOnClickListener(new View.OnClickListener() {
+    }
+    private void Event() {
+        btnDangNhap.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
-            }
-        });
-        btn_signin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+            public void onClick(View view) {
+                progressDialog.show();
+                progressDialog.setContentView(R.layout.layout_loading);
+                progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                String strEmail = edtEmailUser.getText().toString().trim();
+                String strMatKhau = edtMatKhauUser.getText().toString().trim();
+                ArrayList<Admin> arrayList = new ArrayList<>();
+                if (strEmail.length() > 0) {
+                    if (strMatKhau.length() > 0) {
+                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                        db.collection("Admin").get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                            @Override
+                            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                                for (QueryDocumentSnapshot q : queryDocumentSnapshots) {
+                                    arrayList.add(new Admin(q.getString("username"), q.getString("pass")));
+                                }
+                                String tkadmin;
+                                String mkadmin;
+                                int tk = 0;
+                                for (int i = 0; i < arrayList.size(); i++) {
+                                    Admin admin = arrayList.get(i);
+                                    tkadmin = admin.getUsername();
+                                    mkadmin = admin.getPassword();
+                                    sosanh = tkadmin;
+                                    if (strEmail.equals(tkadmin) && strMatKhau.equals(mkadmin)) {
+                                        tk = 1;
+                                        break;
+                                    } else {
+                                        tk = 2;
+                                    }
+                                }
+                                switch (tk) {
+                                    case 1:
+                                        progressDialog.dismiss();
+                                        Intent intent = new Intent(SignInActivity.this, AdminHomeActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                        break;
+                                    case 2:
+                                        FirebaseAuth auth = FirebaseAuth.getInstance();
+                                        auth.signInWithEmailAndPassword(strEmail, strMatKhau).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                                                if (task.isSuccessful()) {
+                                                    Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+                                                    startActivity(intent);
+                                                    finishAffinity();
+                                                    progressDialog.dismiss();
+                                                } else if (!isEmailValid(strEmail) && !sosanh.equals(strEmail)) {
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(SignInActivity.this, "Email định dạng không đúng", Toast.LENGTH_SHORT).show();
+
+                                                } else {
+                                                    progressDialog.dismiss();
+                                                    Toast.makeText(SignInActivity.this, "Tài khoản hoặc mật khẩu không đúng.\nVui lòng kiểm tra lại!", Toast.LENGTH_SHORT).show();
+                                                }
+
+                                            }
+                                        });
+
+                                        break;
+                                }
+                            }
+                        });
+
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(SignInActivity.this, "Bạn chưa nhập mật khẩu", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    progressDialog.dismiss();
+                    Toast.makeText(SignInActivity.this, "Bạn chưa nhập tài khoản", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
+    }
+
+    private boolean isEmailValid(CharSequence email) {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+    @Override
+    public void onBackPressed() {
+        progressDialog.dismiss();
+        // Sự kiện nhấn back 2 lần để out app
+        if (back_pressed + TIME_DELAY > System.currentTimeMillis()) {
+            super.onBackPressed();
+        } else {
+            Toast.makeText(getBaseContext(), "Press once again to exit!",
+                    Toast.LENGTH_SHORT).show();
+        }
+        back_pressed = System.currentTimeMillis();
 
     }
     private void InitWidget() {
@@ -99,12 +202,15 @@ public class SignInActivity extends AppCompatActivity {
         edtEmailUser = findViewById(R.id.edt_email_user);
         edtMatKhauUser = findViewById(R.id.edt_matkhau_user);
         tvForgotPassword = findViewById(R.id.tv_forgot_password);
+
         progressDialog = new ProgressDialog(this);
     }
+
     public void broadcastIntent() {
         registerReceiver(MyReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
 
     }
+
     @Override
     protected void onPause() {
         super.onPause();

33632b4e0eb2c2d9663c6e6954ee3353961d435d
hiepnguyen81 <hiephoang2k486@gmail.com>
2024-11-20 10:47:08 +0700

    edit screen admin add spactivity

--- app/src/main/java/com/example/md06_clothes/View/Admin/AdminAddLoaiSPActivity.java
+++ app/src/main/java/com/example/md06_clothes/View/Admin/AdminAddLoaiSPActivity.java
@@ -49,7 +49,6 @@ public class AdminAddLoaiSPActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_loai_spactivity);
-//
         Init();
         Event();
 

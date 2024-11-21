4d7753e281698a516946d12dc8a04c0fdf5936a1
huynhhnph35237 <huynhhnph35237@fpt.edu.vn>
2024-11-21 14:35:43 +0700

    Huỳnh update

--- app/src/main/java/com/example/md06_clothes/View/FavoriteActivity.java
+++ app/src/main/java/com/example/md06_clothes/View/FavoriteActivity.java
@@ -13,6 +13,6 @@ public class FavoriteActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
     }
-    //
+    //favo
 
 }
